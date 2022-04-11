176370a80080be325d7c2c8b1365fac0db24848b
zkkk-zz <63340410+zkkk-zz@users.noreply.github.com>
2022-04-11 10:30:47 +0800

    Update ssid.js

--- ssid.js
+++ ssid.js
@@ -24,7 +24,7 @@
  */
 
 let config = {
-  silence: false, // 是否静默运行，默认false
+  silence: true, // 是否静默运行，默认false
   cellular: "RULE", // 蜂窝数据下的模式，RULE代表规则模式，PROXY代表全局代理，DIRECT代表全局直连
   wifi: "RULE", // wifi下默认的模式
   all_direct: ["WRT32X", "WRT32X Extreme"], // 指定全局直连的wifi名字
